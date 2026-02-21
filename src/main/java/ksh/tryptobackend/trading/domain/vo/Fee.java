6b245352958418c08d2842f99e7fb6a873c26a4a
kim-se-hee <saree98@naver.com>
2026-02-22 04:54:19 +0900

    feat: CEX 주문 VO 구현

---
+++ src/main/java/ksh/tryptobackend/trading/domain/vo/Fee.java
@@ -0,0 +1,46 @@
+package ksh.tryptobackend.trading.domain.vo;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Objects;
+
+public final class Fee {
+
+    private final BigDecimal amount;
+    private final BigDecimal rate;
+
+    private Fee(BigDecimal amount, BigDecimal rate) {
+        this.amount = amount;
+        this.rate = rate;
+    }
+
+    public static Fee calculate(BigDecimal filledAmount, BigDecimal feeRate) {
+        BigDecimal feeAmount = filledAmount.multiply(feeRate)
+                .setScale(8, RoundingMode.FLOOR);
+        return new Fee(feeAmount, feeRate);
+    }
+
+    public static Fee of(BigDecimal amount, BigDecimal rate) {
+        return new Fee(amount, rate);
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public BigDecimal getRate() {
+        return rate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Fee fee)) return false;
+        return amount.compareTo(fee.amount) == 0 && rate.compareTo(fee.rate) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(amount.stripTrailingZeros(), rate.stripTrailingZeros());
+    }
+}
