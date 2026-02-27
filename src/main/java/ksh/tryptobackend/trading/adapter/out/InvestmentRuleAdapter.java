6af0c5b79b2e3a53c0e9513b15b745cd89b01ca3
kim-se-hee <saree98@naver.com>
2026-02-28 03:34:31 +0900

    refactor: trading InvestmentRule을 ViolationRule로 리네임 - 위반 검증 역할에 맞는 이름으로 변경

--- src/main/java/ksh/tryptobackend/trading/adapter/out/InvestmentRuleAdapter.java
+++ src/main/java/ksh/tryptobackend/trading/adapter/out/InvestmentRuleAdapter.java
@@ -2,7 +2,7 @@ package ksh.tryptobackend.trading.adapter.out;
 
 import ksh.tryptobackend.investmentround.application.port.out.InvestmentRuleQueryPort;
 import ksh.tryptobackend.trading.application.port.out.InvestmentRulePort;
-import ksh.tryptobackend.trading.domain.model.InvestmentRule;
+import ksh.tryptobackend.trading.domain.model.ViolationRule;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -15,9 +15,9 @@ public class InvestmentRuleAdapter implements InvestmentRulePort {
     private final InvestmentRuleQueryPort investmentRuleQueryPort;
 
     @Override
-    public List<InvestmentRule> findByRoundId(Long roundId) {
+    public List<ViolationRule> findByRoundId(Long roundId) {
         return investmentRuleQueryPort.findByRoundId(roundId).stream()
-            .map(info -> InvestmentRule.of(info.ruleId(), info.ruleType(), info.thresholdValue()))
+            .map(info -> ViolationRule.of(info.ruleId(), info.ruleType(), info.thresholdValue()))
             .toList();
     }
 }
