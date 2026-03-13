a0959f145498e9f3f051cc9662eebe522a884a6b
kim-se-hee <saree98@naver.com>
2026-03-14 01:15:39 +0900

    refactor: 포트폴리오 공개 설정 변경 응답에서 userId 제거

--- src/main/java/ksh/tryptobackend/user/adapter/in/dto/response/ChangePortfolioVisibilityResponse.java
+++ src/main/java/ksh/tryptobackend/user/adapter/in/dto/response/ChangePortfolioVisibilityResponse.java
@@ -3,11 +3,10 @@ package ksh.tryptobackend.user.adapter.in.dto.response;
 import ksh.tryptobackend.user.domain.model.User;
 
 public record ChangePortfolioVisibilityResponse(
-    Long userId,
     boolean portfolioPublic
 ) {
 
     public static ChangePortfolioVisibilityResponse from(User user) {
-        return new ChangePortfolioVisibilityResponse(user.getUserId(), user.isPortfolioPublic());
+        return new ChangePortfolioVisibilityResponse(user.isPortfolioPublic());
     }
 }
