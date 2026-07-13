a86fd83cbf9e62f7581f8deb2b8a5e603ba4b91a
kim-se-hee <saree98@naver.com>
2026-07-13 20:30:52 +0900

    fix: 공백만인 피드백 본문 오류 코드를 INVALID_FEEDBACK_LENGTH 로 통일
    - @NotBlank 를 @NotNull 로 완화해 도메인 VO 가 공백 본문을 판정하도록 수정

--- api/src/main/java/ksh/tryptobackend/user/adapter/in/dto/request/SendFeedbackRequest.java
+++ api/src/main/java/ksh/tryptobackend/user/adapter/in/dto/request/SendFeedbackRequest.java
@@ -1,9 +1,9 @@
 package ksh.tryptobackend.user.adapter.in.dto.request;
 
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import ksh.tryptobackend.user.application.port.in.dto.command.SendFeedbackCommand;
 
-public record SendFeedbackRequest(@NotBlank String content) {
+public record SendFeedbackRequest(@NotNull String content) {
 
     public SendFeedbackCommand toCommand(Long authorId) {
         return new SendFeedbackCommand(authorId, content);
